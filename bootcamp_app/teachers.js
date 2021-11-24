891bbd746afb7ecef7603caf49085216ef6273aa
keepitclassick <keepitclassick@gmail.com>
2021-11-24 18:57:15 -0500

    added teach query

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,35 @@
+/*
+  App config
+*/
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+
+const queryString = `
+SELECT DISTINCT teachers.name as name, cohorts.name as cohort
+FROM assistance_requests
+JOIN teachers ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
+ORDER BY name;
+`;
+
+const values = [`${process.argv[2] || 'JUL02'}%`];
+
+pool.query(queryString, values)
+  .then(res => {
+    res.rows.forEach(teacher => {
+      console.log(`${teacher.cohort}: ${teacher.name}`);
+    });
+  })
+  .catch(err => {
+    console.error('query error', err.message);
+  });
